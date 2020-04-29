082fa2cd569a66d94f2055dcfb3078c915081811
Agmar Torres <agmartorres@gmail.com>
2020-04-29 12:59:28 -0300

    COnfiguração do sequelize

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.expres= {
+    host: '127.0.0.1',
+    username:'postgres',
+    password:'docker',
+    database: 'nodeauth',
+    dialect:'postgres',
+    operatorsAliases: false,
+    logging: false,
+    define:{
+        timestramps: true,  // marca a data de atualizaçao
+        underscored: true,
+        underscoredAll: true
+    },
+}
