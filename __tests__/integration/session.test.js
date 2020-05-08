8cb47f4f8c2c9b3fcab931fbd04435918c07e9ba
Agmar Torres <agmartorres@gmail.com>
2020-05-08 18:09:09 -0300

    new update

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,3 +1,11 @@
-it('', () =>{
+const { User } = require('../../src/app/models');
 
+
+describe('Authentication', () => {
+  
+    it('should sum two numbers', async () =>{
+        const user = await User.create({ name: "Agmar", email: "agmartorres@gmail.com", password_hash: "123"})
+        console.log( user )
+        expect(user.email).toBe('agmartorres@gmail.com')
+    })
 })
--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,16 @@
 module.exports= {
+/* 
     host: '127.0.0.1',
     username:'postgres',
     password:'docker',
-    database: 'nodeauth',
-    dialect:'postgres',
+    database: 'nodeauth', 
+*/
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER ,
+    password:process.env.DB_PASS,
+    database:process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT || 'postgres',
+    storage: './__tests__/database.sqlite',
     operatorsAliases: false,
     logging: false,
     define:{
